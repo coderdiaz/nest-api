4cac7f8eca8edec7dbac4778eefce161c5265592
Javier Diaz Chamorro <hello@coderdiaz.me>
2020-03-20 13:55:43 -0600

    feat: added environment

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,12 @@
+import "dotenv/config";
 import { NestFactory } from "@nestjs/core";
 import { AppModule } from "./app.module";
 
 async function bootstrap() {
+  const APP_PORT: number = parseInt(process.env.PORT, 10) || 3000;
+
   const app = NestFactory.create(AppModule);
-  (await app).listen(3000);
+  (await app).listen(APP_PORT);
 }
 
 bootstrap();
